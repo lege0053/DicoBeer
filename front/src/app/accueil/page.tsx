33c2735a90c7d3b04d9c2465bb239658b4f33ce0
Marine Legendre <marine.leg02@gmail.com>
2024-03-09 19:33:43 +0100

    wip: input mail

--- front/src/app/accueil/page.tsx
+++ front/src/app/accueil/page.tsx
@@ -64,12 +64,14 @@ export default function Page() {
         <div className="mt-4 md:mt-0">
           <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 ">Ne ratez plus rien</h2>
           <p className="mb-6 font-light text-gray-500 md:text-lg">Souscrivez à notre newsletter pour recevoir les nouveautés chaque semaine.</p>
+          <div className="mb-6">
+            <input type="text" id="default-input" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Votre adresse mail" />
+          </div>
           <a href="#" className="inline-flex items-center bg-green-300 hover:bg-green-400 focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
             Souscire maintenant
           </a>
         </div>
         <img className="w-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup.svg" alt="dashboard image" />
-        <img className="w-full hidden" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup-dark.svg" alt="dashboard image" />
       </div>
     </div>
   </>);
